b01fd112f9befc640da2ec4a89ec3eb0bb20f1f6
dwd-umd <ddarg@umd.edu>
2019-02-01 00:54:02 -0500

    Move image to AWS S3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
   envName: 'DEV',
   production: false,
   test: false,
-  i18nPrefix: '',
+  i18nPrefix: '/comet-checker',
   versions: {
     app: packageJson.version,
     angular: packageJson.dependencies['@angular/core'],
